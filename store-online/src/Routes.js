0c90294dbee5bf9c13b89a45df64a5dc2756a342
EsGui <guilhermedagama2021100@gmail.com>
2022-10-11 15:06:02 -0300

    feat: arquivo de rotas foi criado e também foi instalado a dependência do react-router-dom

---
+++ store-online/src/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+function Router() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+  
+  export default Router;
