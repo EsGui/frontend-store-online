9c463190407c5c3dafbb1c308e1e8bd17721a966
EsGui <guilhermedagama2021100@gmail.com>
2022-10-11 15:32:03 -0300

    feat: Rota da HomePage foi criada

--- store-online/src/Routes.js
+++ store-online/src/Routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
 
 function Router() {
     return (
       <BrowserRouter>
         <Switch>
-          
+          <Route exact path="/" component={ HomePage } />
         </Switch>
       </BrowserRouter>
     )
