6fd5a97007300baccbe1bbf8144f14efeb292aa6
EsGui <guilhermedagama2021100@gmail.com>
2022-10-13 11:18:29 -0300

    feat: A classe ContainerHomePageHeader foi adicionado para fazer a estilização do header da página inicial

--- store-online/src/Component/Header/HomePageHeader.js
+++ store-online/src/Component/Header/HomePageHeader.js
@@ -5,17 +5,19 @@ import './HomePageHeader.css'
 function HomePageHeader() {
   return (
     <>
-      <div className="TitleHomePageHeader">
-        <h1>Store Online</h1>
-      </div>
-      <div className="SearchHomePageHeader">
-        <input type="search" placeholder="Pesquise algo..." />
-      </div>
-      <div className="MenuHomePageHeader">
-        <Link>Carrinho</Link>
-        <Link>Favoritos</Link>
-        <Link>Compras</Link>
-        <Link>Login</Link>
+      <div className="ContainerHomePageHeader">
+        <div className="TitleHomePageHeader">
+            <h1>Store Online</h1>
+        </div>
+        <div className="SearchHomePageHeader">
+            <input type="search" placeholder="Pesquise algo..." />
+        </div>
+        <div className="MenuHomePageHeader">
+            <Link>Carrinho</Link>
+            <Link>Favoritos</Link>
+            <Link>Compras</Link>
+            <Link>Login</Link>
+        </div>
       </div>
     </>
   )
