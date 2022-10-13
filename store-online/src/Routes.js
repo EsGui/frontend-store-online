63708f2a418ff263fd370051665a831d7e3c50ad
EsGui <guilhermedagama2021100@gmail.com>
2022-10-13 11:40:17 -0300

    feat: A pagina de login foi adicionada as rotas

--- store-online/src/Routes.js
+++ store-online/src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
 
 function Router() {
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ HomePage } />
+          <Route path="/login" component={ LoginPage } />
         </Switch>
       </BrowserRouter>
     )
