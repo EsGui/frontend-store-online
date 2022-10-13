bfb3c2941db5c28a71234cfe78b3925f78422b46
EsGui <guilhermedagama2021100@gmail.com>
2022-10-13 11:50:23 -0300

    feat: tela de login foi construida, mas falta estilização

--- store-online/src/Component/LoginUser/LoginUser.js
+++ store-online/src/Component/LoginUser/LoginUser.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './LoginUser.css'
 
 function LoginUser() {
   return (
     <>
-      <div>
-        <input type="email" placeholder="Digite seu email" />
-        <input type="password" placeholder="Digite sua senha" />
+      <div className="ContainerLoginPage">
+        <div className="UserLoginPage">
+            <input type="email" placeholder="Digite seu email" />
+            <input type="password" placeholder="Digite sua senha" />
+        </div>
       </div>
     </>
   )
