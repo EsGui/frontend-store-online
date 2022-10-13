5a2e315a53d48ce4d361e74c6808aac7cdb37605
EsGui <guilhermedagama2021100@gmail.com>
2022-10-13 13:10:13 -0300

    feat: Imagem gravatar foi adicionado na tela de login

--- store-online/src/Component/LoginUser/LoginUser.js
+++ store-online/src/Component/LoginUser/LoginUser.js
@@ -13,6 +13,7 @@ function LoginUser() {
     <>
       <div className="ContainerLoginPage">
         <div className="UserLoginPage">
+            <img src="https://www.glizzy.nl/wp-content/themes/editium/assets/front/img/def_gravatar.png" alt="Gravatar" />
             <input onChange={ handleSetEmailLogin } type="email" placeholder="Digite seu email" />
             <input onChange={ handleSetPasswordLogin } type="password" placeholder="Digite sua senha" />
             <button onClick={ handleButtonLoginUser } type="button">Entrar</button>
