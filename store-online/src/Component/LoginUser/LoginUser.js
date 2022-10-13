80b287fd3f73f234694966be7fa16639396ca71d
EsGui <guilhermedagama2021100@gmail.com>
2022-10-13 11:38:09 -0300

    feat: O componente de login foi criado

---
+++ store-online/src/Component/LoginUser/LoginUser.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function LoginUser() {
+  return (
+    <>
+      <div>
+        <input type="email" placeholder="Digite seu email" />
+        <input type="password" placeholder="Digite sua senha" />
+      </div>
+    </>
+  )
+}
+
+export default LoginUser;
