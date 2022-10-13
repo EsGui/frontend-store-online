35c1f7485bfe83d60ea530c90900f81227e1a318
EsGui <guilhermedagama2021100@gmail.com>
2022-10-13 11:09:23 -0300

    feat: Header da pagina inicial foi criado

---
+++ store-online/src/Component/Header/HomePageHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function HomePageHeader() {
+  return (
+    <>
+      <div>
+        <h1>Store Online</h1>
+      </div>
+      <div>
+        <input type="search" placeholder="Pesquise algo..." />
+      </div>
+      <div>
+        <Link>Carrinho</Link>
+        <Link>Favoritos</Link>
+        <Link>Compras</Link>
+        <Link>Login</Link>
+      </div>
+    </>
+  )
+}
+
+export default HomePageHeader;
